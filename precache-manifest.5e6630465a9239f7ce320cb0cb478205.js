0dc5c3e7374d3c1f5c371a46b02bdbf6dfe24023
izekki.mohamed@gmail <izekki050619933>
2020-03-22 23:19:09 +0100

    Updates

---
+++ precache-manifest.5e6630465a9239f7ce320cb0cb478205.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d91a5614227e19affc5bea903cec28",
+    "url": "/React-Todo/index.html"
+  },
+  {
+    "revision": "3405bb3b7eb120085a86",
+    "url": "/React-Todo/static/css/main.131e79ed.chunk.css"
+  },
+  {
+    "revision": "c457d94a418d27f23a27",
+    "url": "/React-Todo/static/js/2.030cd99c.chunk.js"
+  },
+  {
+    "revision": "f01cf20f87878beb20bf2f939f8352f1",
+    "url": "/React-Todo/static/js/2.030cd99c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3405bb3b7eb120085a86",
+    "url": "/React-Todo/static/js/main.f4c2f4d8.chunk.js"
+  },
+  {
+    "revision": "b5b08d632e19e0ec5073",
+    "url": "/React-Todo/static/js/runtime-main.9d21ba5c.js"
+  }
+]);
--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./index.css";
-import TodoApp from "./component/TodoApp";
-
-const App = () => {
-  return <TodoApp />;
-};
-
-export default App;
