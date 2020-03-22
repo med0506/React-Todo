cc8f38d5bced27207aa3b232870566f3d985c14a
izekki.mohamed@gmail <izekki050619933>
2020-03-22 23:31:49 +0100

    Updates

--- precache-manifest.0facd57ccf858b70d5249acb9ec993fc.js
+++ precache-manifest.0facd57ccf858b70d5249acb9ec993fc.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "75d91a5614227e19affc5bea903cec28",
+    "revision": "37bb760fd2abf9be4af545d4fdb1ed8b",
     "url": "/React-Todo/index.html"
   },
   {
-    "revision": "3405bb3b7eb120085a86",
+    "revision": "25272c7493baad8d143f",
     "url": "/React-Todo/static/css/main.131e79ed.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/React-Todo/static/js/2.030cd99c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3405bb3b7eb120085a86",
-    "url": "/React-Todo/static/js/main.f4c2f4d8.chunk.js"
+    "revision": "25272c7493baad8d143f",
+    "url": "/React-Todo/static/js/main.452fc6ec.chunk.js"
   },
   {
     "revision": "b5b08d632e19e0ec5073",
